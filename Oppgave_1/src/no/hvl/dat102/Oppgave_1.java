2623cb38091001e86ac18301918c715bd742f187
Stian Grønås <42640705+hvl578007@users.noreply.github.com>
2019-03-12 14:15:32 +0100

    oppgåve 1 + begynt på 2

---
+++ Oppgave_1/src/no/hvl/dat102/Oppgave_1.java
@@ -0,0 +1,47 @@
+﻿package no.hvl.dat102;
+
+import java.time.*;
+
+public class Oppgave_1 {
+
+    public static void main(String[] args) {
+        Instant foerN = Instant.now();
+        System.out.println(sumN(10)); //tester a)
+        Instant etterN = Instant.now();
+
+        Duration durN = Duration.between(foerN, etterN);
+        System.out.println("Tid brukt på summen av naturlege tal: " + durN);
+
+        System.out.println();
+
+        //main-program for b)
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println(tallFolge(i));
+        }
+    }
+
+    private static void a() {
+
+    }
+
+    // a)
+    public static int sumN(int n) {
+        if(n <= 1) { //evt ha med n < 1 -> return 0
+            return 1;
+        } else {
+            return sumN(n-1) + n;
+        }
+    }
+
+    // b)
+    public static int tallFolge(int n) {
+        if(n <= 0) { //evt ha med n < 0 -> return 0
+            return 2;
+        } else if(n == 1) {
+            return 5;
+        } else {
+            return 5 * tallFolge(n-1) - 6 * tallFolge(n-2) + 2;
+        }
+    }
+}
---
+++ Oppgave_1/src/no/hvl/dat102/SolveTowers.java
@@ -0,0 +1,42 @@
+﻿package no.hvl.dat102;
+
+import java.time.*;
+
+/**
+ * SolveTowers uses recursion to solve the Towers of Hanoi puzzle.
+ *
+ * @author Lewis and Chase
+ * @version 4.0
+ */
+public class SolveTowers
+{
+    /**
+     * Creates a TowersOfHanoi puzzle and solves it.
+     */
+    public static void main(String[] args) throws InterruptedException
+    {
+        TowersOfHanoi towers = new TowersOfHanoi(64);
+        Instant foer = Instant.now();
+        towers.solve();
+        Instant etter = Instant.now();
+
+        Duration d = Duration.between(foer, etter);
+
+        System.out.println("Før: " + foer + ", etter: " + etter + ".\nTid brukt: " + d);
+
+        Thread.sleep(10000);
+
+        towers = new TowersOfHanoi(32);
+        Instant foer2 = Instant.now();
+        towers.solve();
+        Instant etter2 = Instant.now();
+
+        Duration d2 = Duration.between(foer2, etter2);
+
+        System.out.println("Før: " + foer2 + ", etter: " + etter2 + ".\nTid brukt: " + d2);
+
+        System.out.println("(2^32 - 1) / (2^16 - 1) = " + (Math.pow(2, 32) - 1) / (Math.pow(2, 16) - 1) );
+
+        System.out.println("Tid32 / Tid 16 = " + d.toMillis() / d2.toMillis());
+    }
+}
